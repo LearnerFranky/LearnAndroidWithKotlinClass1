bab963dc52e0115898321c8b47476011395bf1dc
Franky <542686505@qq.com>
2021-06-15 23:39:18 +0800

    feat: Act消失后的数据保存

--- app/src/main/java/com/nr97/activitytest/MainActivity.kt
+++ app/src/main/java/com/nr97/activitytest/MainActivity.kt
@@ -12,7 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(tag,"onCreat")
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null){
+            val tempData = savedInstanceState.getString("data_key")
+            Log.d(tag,"tempData is $tempData")
+        }
         val mainActBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActBinding.root)
         mainActBinding.startNormalActivity.setOnClickListener {
@@ -54,5 +59,11 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Log.d(tag, "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState:Bundle){
+        super.onSaveInstanceState(outState)
+        val tempData = "Something you just typed"
+        outState.putString("datd_key",tempData)
     }
 }
